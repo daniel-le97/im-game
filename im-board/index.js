aa300e8560b35df1d158f7446bb4b787bd4b5871
daniel <daniel97.le@gmail.com>
2022-12-19 13:02:30 -0700

    hey

--- im-board/index.js
+++ im-board/index.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 // @ts-ignore
 // eslint-disable-next-line no-global-assign
 // hello
-// require = require('esm')(module)
+require = require('esm')(module)
 module.exports = require('./server/main')
